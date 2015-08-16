f01b1a0c2882f73de1167fc489bb5cffee5b3939
Wolfgang Barth <wob@swobspace.net>
2015-08-16 10:14:40 +0200

    add admin and admin role

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# -- load YAML config data
+config = YAML::load_file(File.join(Rails.root, 'db', 'seeds.yml'))
+
+# -- Roles from yaml(:uniqueness => true)
+Wobauth::Role.create(config['roles'])
+
+# -- start with Admin
+admin_role = Wobauth::Role.find_by_name("Admin")
+
+Wobauth::User.transaction do
+  admin = Wobauth::User.find_or_create_by(username: 'admin') do |adm|
+	    adm.sn = 'Admin'
+	    adm.givenname = 'Meister'
+	    adm.password = 'admin9999'
+	    adm.password_confirmation = 'admin9999'
+	  end
+
+  Wobauth::Authority.transaction do
+    a = Wobauth::Authority.find_or_create_by(authorizable: admin, role: admin_role)
+    raise ActiveRecord::Rollback if a.nil?
+  end
+end
+
+
