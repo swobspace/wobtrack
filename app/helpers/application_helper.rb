c3a83caeff8ff21b9a935cd69eef44ba44df2385
Wolfgang Barth <wob@swobspace.net>
2015-08-16 10:17:06 +0200

    navigation view helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 module ApplicationHelper
   include Wobapphelpers::Helpers::All
+
+    def configuration_active_class
+      if Wobtrack::CONFIGURATION_CONTROLLER.include?(controller.controller_name.to_s)
+        "active"
+      end
+    end
+
 end
